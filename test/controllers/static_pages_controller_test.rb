704ba73f59ec594422a44b8e7bd817a4062d4215
Guilherme Figueiroa <guilherme.mfigueiroa@gmail.com>
2015-04-21 16:04:50 -0300

    Correções e tradução

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -7,21 +7,15 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_select "title", "Ruby on Rails Tutorial Sample App"
   end
 
-  test "should get help" do
-    get :help
-    assert_response :success
-    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
-  end
-
 test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Sobre | Ruby on Rails Tutorial Sample App"
   end
 
 test "should get contact" do
     get :contact
     assert_response :success
-    assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Contato | Ruby on Rails Tutorial Sample App"
   end
 end
