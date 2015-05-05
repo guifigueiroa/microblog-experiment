24ff7f3494e423385087fd80b634347a5e052e31
Guilherme Figueiroa <guilherme.mfigueiroa@gmail.com>
2015-05-04 22:54:36 -0300

    Finish user edit, update, index and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
 
   def index
     @users = User.paginate(page: params[:page])
@@ -39,24 +40,37 @@ class UsersController < ApplicationController
     end
   end
 
-  # Before filters
-
-  # Confirms a logged-in user.
-  def logged_in_user
-    unless logged_in?
-      store_location
-      flash[:danger] = "Por favor faça login"
-      redirect_to login_url
-    end
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "Usuário excluído"
+    redirect_to users_url
   end
 
-  # Confirms correct user
-  def correct_user
-    @user = User.find(params[:id])
-    redirect_to(root_url) unless current_user?(@user)
-  end
+  
 
   private
+    # Confirms a logged-in user.
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "Por favor faça login"
+        redirect_to login_url
+      end
+    end
+
+    # Before filters
+    
+    # Confirms correct user
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
+
+    # Confirms an admin user.
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end
+
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
